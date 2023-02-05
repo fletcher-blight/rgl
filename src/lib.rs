44c82ae785b98cceaf027d1dccb0c237368848e6
Fletcher Blight <fletcher.blight@gmail.com>
2023-02-05 19:01:29 +1000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,208 @@
+use gl::types::*;
+
+#[repr(transparent)]
+#[derive(Debug, Clone, Copy)]
+pub struct Buffer(GLuint);
+
+#[derive(Debug, Clone, Copy)]
+pub enum BufferBindingTarget {
+    /// Vertex attributes
+    Array,
+
+    /// Atomic counter storage
+    AtomicCounter,
+
+    /// Buffer copy source
+    CopyRead,
+
+    /// Buffer copy destination
+    CopyWrite,
+
+    /// Indirect compute dispatch commands
+    DispatchIndirect,
+
+    /// Indirect command arguments
+    DrawIndirect,
+
+    /// Vertex array indices
+    ElementArray,
+
+    /// Pixel read target
+    PixelPack,
+
+    /// Texture data source
+    PixelUnpack,
+
+    /// Query result buffer
+    Query,
+
+    /// Read-write storage for shaders
+    ShaderStorage,
+
+    /// Texture data buffer
+    Texture,
+
+    /// Transform feedback buffer
+    TransformFeedbackBuffer,
+
+    /// Uniform block storage
+    Uniform,
+}
+
+impl From<BufferBindingTarget> for GLenum {
+    fn from(value: BufferBindingTarget) -> Self {
+        match value {
+            BufferBindingTarget::Array => gl::ARRAY_BUFFER,
+            BufferBindingTarget::AtomicCounter => gl::ATOMIC_COUNTER_BUFFER,
+            BufferBindingTarget::CopyRead => gl::COPY_READ_BUFFER,
+            BufferBindingTarget::CopyWrite => gl::COPY_WRITE_BUFFER,
+            BufferBindingTarget::DispatchIndirect => gl::DISPATCH_INDIRECT_BUFFER,
+            BufferBindingTarget::DrawIndirect => gl::DRAW_INDIRECT_BUFFER,
+            BufferBindingTarget::ElementArray => gl::ELEMENT_ARRAY_BUFFER,
+            BufferBindingTarget::PixelPack => gl::PIXEL_PACK_BUFFER,
+            BufferBindingTarget::PixelUnpack => gl::PIXEL_UNPACK_BUFFER,
+            BufferBindingTarget::Query => gl::QUERY_BUFFER,
+            BufferBindingTarget::ShaderStorage => gl::SHADER_STORAGE_BUFFER,
+            BufferBindingTarget::Texture => gl::TEXTURE_BUFFER,
+            BufferBindingTarget::TransformFeedbackBuffer => gl::TRANSFORM_FEEDBACK_BUFFER,
+            BufferBindingTarget::Uniform => gl::UNIFORM_BUFFER,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ErrorBindBuffer {
+    /// `buffer` is not a name previously returned from a call to [gen_buffers]
+    InvalidBuffer(Buffer),
+}
+
+/// bind a named buffer object
+///
+/// Binds a buffer object to the specified buffer binding point. Calling [bind_buffer] with `buffer`
+/// set to the name of a buffer object binds that buffer object name to the `target`. If no buffer
+/// object with name `buffer` exists, one is created with that name. When a buffer object is bound
+/// to a target, the previous binding for that target is automatically broken.
+///
+/// Buffer object names are unsigned integers. The value zero is reserved, but there is no default
+/// buffer object for each buffer object target. Instead, `buffer` set to zero effectively unbinds
+/// any buffer object previously bound, and restores client memory usage for that buffer object target
+/// (if supported for that target). Buffer object names and the corresponding buffer object contents
+/// are local to the shared object space of the current GL rendering context;
+/// two rendering contexts share buffer object names only if they explicitly enable sharing between
+/// contexts through the appropriate GL windows interfaces functions.
+///
+/// [gen_buffers] must be used to generate a set of unused buffer object names.
+///
+/// The state of a buffer object immediately after it is first bound is an unmapped zero-sized memory
+/// buffer with [ReadWrite] access and [StaticDraw] usage.
+///
+/// While a non-zero buffer object name is bound, GL operations on the target to which it is bound
+/// affect the bound buffer object, and queries of the target to which it is bound return state from
+/// the bound buffer object. While buffer object name zero is bound, as in the initial state,
+/// attempts to modify or query state on the target to which it is bound generates an
+/// [InvalidOperation](ErrorOpenGL::InvalidOperation) error.
+///
+/// A buffer object binding created with [bind_buffer] remains active until a different buffer
+/// object name is bound to the same target, or until the bound buffer object is deleted with
+/// [delete_buffers].
+///
+/// Once created, a named buffer object may be re-bound to any target as often as needed. However,
+/// the GL implementation may make choices about how to optimize the storage of a buffer object based
+/// on its initial binding target.
+///
+/// # Arguments
+/// * `target` - Specifies the target to which the buffer object is bound
+/// * `buffer` - Specifies the name of a buffer object
+///
+/// # Target Usage
+/// - When a non-zero buffer object is bound to the [Array](BufferBindingTarget::Array) target,
+/// the vertex array pointer parameter is interpreted as an offset within the buffer object measured
+/// in basic machine units.
+/// - While a non-zero buffer object is bound to the [ElementArray](BufferBindingTarget::ElementArray)
+/// target, the indices parameter of [draw_elements], [draw_elements_instanced], [draw_elements_base_vertex],
+/// [draw_range_elements], [draw_range_elements_base_vertex], [multi_draw_elements], or
+/// [multi_draw_elements_base_vertex] is interpreted as an offset within the buffer object measured
+/// in basic machine units.
+///
+/// # Compatability
+/// - 3.1 or greater is required for: [CopyRead](BufferBindingTarget::CopyRead),
+/// [Uniform](BufferBindingTarget::Uniform), [Texture](BufferBindingTarget::Texture)
+/// - 4.2 or greater is required for: [AtomicCounter](BufferBindingTarget::AtomicCounter)
+/// - 4.3 or greater is required for: [DispatchIndirect](BufferBindingTarget::DispatchIndirect) and
+/// [ShaderStorage](BufferBindingTarget::ShaderStorage)
+/// - 4.4 or greater is required for: [Query](BufferBindingTarget::Query)
+pub fn bind_buffer(target: BufferBindingTarget, buffer: Buffer) -> Result<(), ErrorBindBuffer> {
+    unsafe { gl::BindBuffer(target.into(), buffer.0) };
+    match get_error() {
+        ErrorOpenGL::NoError => Ok(()),
+        ErrorOpenGL::InvalidValue => Err(ErrorBindBuffer::InvalidBuffer(buffer)),
+        _ => unreachable!(),
+    }
+}
 
 /// Currently defined OpenGL errors
+#[derive(Debug)]
 pub enum ErrorOpenGL {
     /// No error has been recorded
     NoError,
 
-    /// An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.
+    /// An unacceptable value is specified for an enumerated argument. The offending command is
+    /// ignored and has no other side effect than to set the error flag
     InvalidEnum,
 
-    /// A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.
+    /// A numeric argument is out of range. The offending command is ignored and has no other
+    /// side effect than to set the error flag
     InvalidValue,
 
-    /// The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.
+    /// The specified operation is not allowed in the current state. The offending command is
+    /// ignored and has no other side effect than to set the error flag
     InvalidOperation,
 
-    /// The framebuffer object is not complete. The offending command is ignored and has no other side effect than to set the error flag.
+    /// The framebuffer object is not complete. The offending command is ignored and has no other
+    /// side effect than to set the error flag
     InvalidFrameBufferOperation,
 
-    /// There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.
+    /// There is not enough memory left to execute the command. The state of the GL is undefined,
+    /// except for the state of the error flags, after this error is recorded
     OutOfMemory,
 
-    /// An attempt has been made to perform an operation that would cause an internal stack to underflow.
+    /// An attempt has been made to perform an operation that would cause an internal stack to underflow
     StackUnderflow,
 
-    /// An attempt has been made to perform an operation that would cause an internal stack to overflow.
+    /// An attempt has been made to perform an operation that would cause an internal stack to overflow
     StackOverflow,
 
-    /// GL returned a non-standard error code.
+    /// GL returned a non-standard error code
     Unknown(u32),
 }
 
 /// return error information
 ///
 /// Returns the value of the error flag. Each detectable error is assigned a numeric code and symbolic name.
-/// When an error occurs, the error flag is set to the appropriate error code value.
-/// No other errors are recorded until [get_error](get_error) is called, the error code is returned, and the flag is reset to [NoError](ErrorOpenGL::NoError).
-/// If a call to [get_error](get_error) returns [NoError](ErrorOpenGL::NoError),
-/// there has been no detectable error since the last call to [get_error](get_error),
+/// When an error occurs, the error flag is set to the appropriate error code value. No other errors
+/// are recorded until [get_error] is called, the error code is returned, and the flag is reset
+/// to [NoError](ErrorOpenGL::NoError). If a call to [get_error] returns
+/// [NoError](ErrorOpenGL::NoError), there has been no detectable error since the last call to [get_error],
 /// or since the GL was initialized.
 ///
-/// To allow for distributed implementations, there may be several error flags. If any single error flag has recorded an error,
-/// the value of that flag is returned and that flag is reset to [NoError](ErrorOpenGL::NoError) when [get_error](get_error) is called.
-/// If more than one flag has recorded an error, [get_error](get_error) returns and clears an arbitrary error flag value.
-/// Thus, [get_error](get_error) should always be called in a loop, until it returns [NoError](ErrorOpenGL::NoError), if all error flags are to be reset.
+/// To allow for distributed implementations, there may be several error flags. If any single
+/// error flag has recorded an error, the value of that flag is returned and that flag is reset to
+/// [NoError](ErrorOpenGL::NoError) when [get_error] is called. If more than one flag has recorded
+/// an error, [get_error] returns and clears an arbitrary error flag value. Thus, [get_error] should
+/// always be called in a loop, until it returns [NoError](ErrorOpenGL::NoError),
+/// if all error flags are to be reset.
 ///
-/// When an error flag is set, results of a GL operation are undefined only if [OutOfMemory](ErrorOpenGL::OutOfMemory) has occurred.
-/// In all other cases, the command generating the error is ignored and has no effect on the GL state or frame buffer contents.
-/// If the generating command returns a value, it returns 0.
-/// If [get_error](get_error) itself generates an error, it returns 0.
+/// When an error flag is set, results of a GL operation are undefined only if
+/// [OutOfMemory](ErrorOpenGL::OutOfMemory) has occurred. In all other cases, the command generating
+/// the error is ignored and has no effect on the GL state or frame buffer contents. If the generating
+/// command returns a value, it returns 0. If [get_error] itself generates an error, it returns 0.
 pub fn get_error() -> ErrorOpenGL {
     match unsafe { gl::GetError() } {
         gl::NO_ERROR => ErrorOpenGL::NoError,
         gl::INVALID_ENUM => ErrorOpenGL::InvalidEnum,
         gl::INVALID_VALUE => ErrorOpenGL::InvalidValue,
         gl::INVALID_OPERATION => ErrorOpenGL::InvalidOperation,
-        gl::INVALID_FRAME_BUFFER_OPERATION => ErrorOpenGL::InvalidFrameBufferOperation,
+        gl::INVALID_FRAMEBUFFER_OPERATION => ErrorOpenGL::InvalidFrameBufferOperation,
         gl::OUT_OF_MEMORY => ErrorOpenGL::OutOfMemory,
         gl::STACK_UNDERFLOW => ErrorOpenGL::StackUnderflow,
         gl::STACK_OVERFLOW => ErrorOpenGL::StackUnderflow,
@@ -61,88 +210,4 @@ pub fn get_error() -> ErrorOpenGL {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// pub mod uniform;
-//
-// pub struct Pipeline(GLuint);
-// pub struct Program(GLuint);
-//
-// pub enum ErrorOpenGL {
-//     /// An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.
-//     InvalidEnum,
-//
-//     /// A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.
-//     InvalidValue,
-//
-//     /// The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.
-//     InvalidOperation,
-//
-//     /// The framebuffer object is not complete. The offending command is ignored and has no other side effect than to set the error flag.
-//     InvalidFrameBufferOperation,
-//
-//     /// There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.
-//     OutOfMemory,
-//
-//     /// An attempt has been made to perform an operation that would cause an internal stack to underflow.
-//     StackUnderflow,
-//
-//     /// An attempt has been made to perform an operation that would cause an internal stack to overflow.
-//     StackOverflow,
-// }
-//
-// /// return error information
-// ///
-// /// Returns the value of the error flag. Each detectable error is assigned a numeric code and symbolic name.
-// /// When an error occurs, the error flag is set to the appropriate error code value.
-// /// No other errors are recorded until [get_error](get_error) is called, the error code is returned, and the flag is reset to `None`.
-// /// If a call to [get_error](get_error) returns `None`, there has been no detectable error since the last call to [get_error](get_error), or since the GL was initialized.
-// pub fn get_error() -> Option<ErrorOpenGL> {
-//     None
-// }
-//
-// pub enum ErrorActiveShaderProgram {
-//     Unknown(ErrorOpenGL),
-//
-// }
-//
-// /// set the active program object for a program pipeline object
-// ///
-// ///
-// /// Sets the linked program named by `program` to be the active program for the program pipeline object `pipeline`.
-// /// The active program in the active program pipeline object is the target of calls to [uniform](uniform) when no program has been made current through a call to [use_program](use_program).
-// ///
-// /// # Arguments
-// /// * `pipeline` - Specifies the program pipeline object to set the active program object for.
-// /// * `program` - Specifies the program object to set as the active program pipeline object pipeline.
-// pub fn active_shader_program(pipeline: Pipeline, program: Program) -> Result<(), ErrorActiveShaderProgram> {
-//     Ok(())
-// }
-//
-//
-//
-// pub enum ErrorUseProgram {
-//
-// }
-//
-// pub fn use_program() -> Result<(), ErrorUseProgram> {
-//     Ok(())
-// }
+pub use gl::load_with;
