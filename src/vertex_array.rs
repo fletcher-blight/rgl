19409953be87838dbf073544fac97c35d5881ef7
Fletcher Blight <fletcher.blight@gmail.com>
2023-03-07 12:52:40 +1000

    vertex_array: delete_vertex_arrays

--- src/vertex_array.rs
+++ src/vertex_array.rs
@@ -55,5 +55,44 @@ pub struct VertexArray(pub u32);
 /// * [vertex_attrib_pointer]
 pub fn bind_vertex_array(array: VertexArray) {
     let array = array.0;
+
+    // SAFE: synchronous integer copy
     unsafe { gl::BindVertexArray(array) }
+}
+
+/// # Delete vertex array objects
+/// <https://registry.khronos.org/OpenGL-Refpages/gl4/html/glDeleteVertexArrays.xhtml>
+///
+/// # Arguments
+/// * `arrays` - Specifies the slice arrays containing the names of the objects to be deleted
+///
+/// # Example
+/// ```no_run
+/// # use rgl::prelude::*;
+/// delete_vertex_arrays(&[VertexArray(42), VertexArray(7)]);
+/// ```
+///
+/// # Description
+/// [delete_vertex_arrays] deletes all vertex array objects whose names are stored in the slice
+/// `arrays`. Once a vertex array object is deleted it has no contents and its name is again unused.
+/// If a vertex array object that is currently bound is deleted, the binding for that object reverts
+/// to zero and the default vertex array becomes current. Unused names in `arrays` are silently
+/// ignored, as is the value zero.
+///
+/// # Version Support
+///
+/// | Function / Feature Name | 2.0 | 2.1 | 3.0 | 3.1 | 3.2 | 3.3 | 4.0 | 4.1 | 4.2 | 4.3 | 4.4 | 4.5 |
+/// |-------------------------|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|
+/// | [delete_vertex_arrays] | N | N | Y | Y | Y | Y | Y | Y | Y | Y | Y | Y |
+///
+/// # See Also
+/// * [gen_vertex_arrays]
+/// * [is_vertex_array]
+/// * [bind_vertex_array]
+pub fn delete_vertex_arrays(arrays: &[VertexArray]) {
+    let n = arrays.len() as GLsizei;
+    let arrays = arrays.as_ptr() as *const u32;
+
+    // SAFE: synchronous read of `array`, nothing retained
+    unsafe { gl::DeleteVertexArrays(n, arrays) }
 }
