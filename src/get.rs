09f79fb99f00d248c24ba302227916614e9c5606
Fletcher Blight <fletcher.blight@gmail.com>
2023-03-14 08:12:38 +1000

    get: active_texture documentation wip

--- src/get.rs
+++ src/get.rs
@@ -1,4 +1,4 @@
-//! Return the value or values of a selected parameter
+//! # Return the value or values of a selected parameter
 //! <https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGet.xhtml>
 
 use crate::prelude::*;
@@ -36,6 +36,33 @@ fn get_i32(pname: GLenum) -> i32 {
     val
 }
 
+/// # Return the value or values of a selected parameter
+/// <https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGet.xhtml>
+///
+/// # Example
+/// ```no_run
+/// # use rgl::prelude::*;
+/// active_texture(42);
+/// assert_eq!(get_active_texture(), 42);
+/// ```
+///
+/// # Description
+/// Returns a single value indicating the active multitexture unit. The initial value is `0``. See
+/// [active_texture].
+///
+/// # Compatability
+///
+/// # Errors
+///
+/// # Associated Gets
+///
+/// # Version Support
+///
+/// | Function / Feature Name | 2.0 | 2.1 | 3.0 | 3.1 | 3.2 | 3.3 | 4.0 | 4.1 | 4.2 | 4.3 | 4.4 | 4.5 |
+/// |-------------------------|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|
+/// | [get_active_texture] | N | N | Y | Y | Y | Y | Y | Y | Y | Y | Y | Y |
+///
+/// # See Also
 pub fn get_active_texture() -> u32 {
     get_i32(gl::ACTIVE_TEXTURE) as u32
 }
